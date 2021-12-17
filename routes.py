42de25baed7584e19ae64514475cd2d3b9ef1815
Abhishek Dubey <33081271+spdubey@users.noreply.github.com>
2021-12-17 16:44:08 +0530

    Added doc string

--- routes.py
+++ routes.py
@@ -4,23 +4,15 @@ app = Flask(__name__)
 func_list = ['square', 'cube']
 
 
-
-
-
-
-
 @app.route('/')
 def home():
+    """Activate the index.html and display it as soon as the program run"""
     return render_template('index.html', func_list=func_list)
-
-
-
-
-
 
 
 @app.route('/run', methods=['GET', 'POST'])
 def run():
+    """Creating an endpoint (fancy name for page /run)"""
     func = request.form['function_list']
     input_text = request.form['input_box']
     # This is where calculation happens
@@ -29,7 +21,9 @@ def run():
                            input_text=input_text, output_text=output,
                            func_list=func_list)
 
+
 def calc_fun(func, input_text):
+    """Helper function to execute functionality on the UI"""
     if 'square' in func.lower():
         return f"The square of the number {input_text} is  : {math.pow(eval(input_text), 2)}"
     elif 'cube' in func.lower():
